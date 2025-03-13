1e6ea12542a6beb25cfcc6b72bc89d8db5f54f0e
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-03-13 11:51:52 -0400

    building

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+const aboutRoute = require('./routes/about');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// setting view engine as ejs
+app.set('view-engine', 'ejs')
+
+app.use('/about', aboutRoute);
 
 app.get('/', (req, res) => {
      res.send("This is the home page")
 });
 
 
-app.listen(port, () => {
+app.listen(PORT, () => {
      console.log(`Server is running on port ${PORT}`);
 });
 
